7dbce74a92be8b8725d09650025c6f8a5d5a9da2
Extraterrestial-Annie <j.j.karkkainen@gmail.com>
2022-01-06 23:47:23 +0200

    Update App.js

--- part1/1.1-1.5/src/App.js
+++ part1/1.1-1.5/src/App.js
@@ -1,5 +1,40 @@
 import React from 'react'
 
+
+const Header = (props) => {
+  return(
+    <h1>{props.course.name}</h1>
+  )
+}
+
+const Content = (props) => {
+  return(
+    <div>
+      <Part part={props.course.parts[0]} />
+      <Part part={props.course.parts[1]} />
+      <Part part={props.course.parts[2]} />
+    </div>
+  )
+}
+
+const Part = (props) => {
+  return(
+    <p>{props.part.name} {props.part.exercises} </p>
+  )
+}
+
+const Total = (props) => {
+  return(
+    <p>Number of exercises {
+      props.course.parts[0].exercises + 
+      props.course.parts[1].exercises + 
+      props.course.parts[2].exercises
+      } 
+    </p>
+  )
+
+}
+
 const App = () => {
   const course = {
     name: 'Half Stack application development',
@@ -26,35 +61,6 @@ const App = () => {
       <Total course={course}/>
     </div>
   )
-}
-
-const Header = (props) => {
-  return(
-    <h1>{props.course.name}</h1>
-  )
-}
-
-const Content = (props) => {
-  return(
-    <div>
-      <Part part={props.course.parts[0]} />
-      <Part part={props.course.parts[1]} />
-      <Part part={props.course.parts[2]} />
-    </div>
-  )
-}
-
-const Part = (props) => {
-  return(
-    <p>{props.part.name} {props.part.exercises} </p>
-  )
-}
-
-const Total = (props) => {
-  return(
-    <p>Number of exercises {props.course.parts[0].exercises + props.course.parts[1].exercises + props.course.parts[2].exercises} </p>
-  )
-
 }
 
 export default App
